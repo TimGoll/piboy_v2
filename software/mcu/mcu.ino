3ae2ebc2469b209f3420fa3583aded360f56b5d5
TimDerGoll <tim@timgoll.de>
2018-07-02 18:03:25 +0200

    continued work on MCU software

--- software/mcu/mcu.ino
+++ software/mcu/mcu.ino
@@ -4,13 +4,14 @@
 #include "Wire.h"
 
 /*
- * There are three different states:
- *(0: LOW PWR  : low power mode)
- * 1: CHARGING : displays current charging state on the LEDs
- * 2: RPI_ON   : raspbery pi is on and the MAX17043 IC monitors the current voltage
- * 3: BAT_TEST : display current battery voltage state
+ * There are four different states:
+ *(0: LOW PWR           : low power mode)
+ * 1: CHARGING          : displays current charging state on the LEDs
+ * 2: CHARGING_FINISHED : lets all LEDs blink
+ * 3: RPI_ON            : raspbery pi is on and the MAX17043 IC monitors the current voltage
+ * 4: BAT_TEST          : display current battery voltage state
  *
- * There are four different events:
+ * There are five different events:
  * 10: CHARGING STOPPED
  * 11: CHARGING STARTED
  * 20: RPI_PWR_OFF
@@ -18,48 +19,78 @@
  * 31: REQUEST_BAT_STATE
  */
 
-#define BAUDRATE 115200
-#define THRESHOLD 30
+#define BAUDRATE 115200         //baudrate for uart between raspberry and arduino
+#define THRESHOLD 30            //debouncetime for buttons
+#define SHUTDOWN_TIMEOUT = 1*60 //timeout for shutdown response
 
 //interrupt pins are able to awake the mcu from its low power mode
 #define PIN_INT_BATTEST 2
 #define PIN_INT_ONOFF 3
 #define PIN_INT_CHARGE 999
 
-uint8_t led_state = 0;
-#define LED_STATE_DEFAULT 0
-#define LED_STATE_BATTEST 1
-#define LED_STATE_CHARGING 2
-uint8_t led_state_event_flag = false;
-uint32_t last_led_state_time = 0;
+//state IDs
+#define STATE_LOW_PWR 0
+#define STATE_CHARGING 1
+#define STATE_CHARGING_FINISHED 2
+#define STATE_RPI_ON 3
+#define STATE_BAT_TEST 4
 
-uint8_t rpi_state = 0;
-#define RPI_STATE_POWEROFF 0
-#define RPI_STATE_POWERON 1
-uint8_t rpi_state_event_flag = false;
-uint32_t last_rpi_state_time = 0;
+//event flags
+bool event_charging_started       = false;
+bool event_charging_stoppend      = false;
+bool event_rpi_power_on           = false;
+bool event_rpi_power_off          = false;
+bool event_request_bat_state      = false;
 
-#define SHUTDOWN_TIMEOUT = 1*60
-uint32_t shutdown_timeout = 0;
+//timing variables
+uint32_t time_last_bat_state_event = 0;
+uint32_t time_last_on_off_event    = 0;
+
+uint32_t time_shutdown_timeout     = 0;
+
+/******************************************************************************/
+/* INTERRUPT CALLBACK FUNCTIONS ***********************************************/
+/******************************************************************************/
 
 void int_battest() { //negative edge (inverted logic)
-    if (millis() - last_led_state_time > THRESHOLD) {
-        last_led_state_time = millis();
-        led_state_event_flag = true;
+    if (millis() - time_last_bat_state_event > THRESHOLD) {
+        time_last_bat_state_event = millis();
+        event_request_bat_state   = true;
     }
 }
 
 void int_onoff() { //positive & negative edge
-    if (millis() - last_rpi_state_time > THRESHOLD) {
-        last_rpi_state_time = millis();
-        rpi_state = digitalRead(PIN_INT_ONOFF);
-        rpi_state_event_flag = true;
+    if (millis() - time_last_on_off_event > THRESHOLD) {
+        time_last_on_off_event = millis();
+        if (digitalRead(PIN_INT_ONOFF))
+            event_rpi_power_on = true;
+        else
+            event_rpi_power_off = true;
     }
 }
 
 void int_charge() {
 
 }
+
+/******************************************************************************/
+/* EVENT HANDLE FUNCTIONS *****************************************************/
+/******************************************************************************/
+
+void handle_event_request_bat_state() {
+    event_request_bat_state = false;
+
+    if (state == STATE_CHARGING)
+        return;
+    if (state == STATE_CHARGING_FINISHED)
+        return;
+
+    state = STATE_BAT_TEST;
+}
+
+/******************************************************************************/
+/* MAIN FUNCTIONS *************************************************************/
+/******************************************************************************/
 
 void setup() {
     //setup serial communication with the Raspberry Pi
@@ -72,10 +103,11 @@ void setup() {
     //attach interrupts; interrupts stop low power mode
     attachInterrupt(PIN_INT_BATTEST, int_battest, LOW);
     attachInterrupt(PIN_INT_ONOFF,   int_onoff,   CHANGE);
+    //TODO more wake ups
 
     //setup fuel gauge
     Wire.begin();
-    batteryMonitor.reset();
+    batteryMonitor.reset(); //TODO reset needed?
     batteryMonitor.quickStart();
 
     //enter low power mode
@@ -83,25 +115,24 @@ void setup() {
 }
 
 void loop() {
-    // CHECK INTERRUPT FLAGS
-    if (led_state_event_flag) {
-        // do not display led_state when it's already charging
+    // CHECK EVENT FLAGS
+    if (event_request_bat_state) {
+        handle_event_request_bat_state();
     }
 
-    if (rpi_state_event_flag) {
-        rpi_state_event_flag = false;
-        if (rpi_state == RPI_STATE_POWEROFF) {
-            //send shutdown command to the raspberry and set shutdown timeout variable
-            Serial.println("SHUTDOWN");
-            shutdown_timeout = millis() + SHUTDOWN_TIMEOUT;
-        } else if (rpi_state == RPI_STATE_POWERON) {
+    // SWITCH STATES
+    if (state == STATE_LOW_PWR) {
+        // state 0 is the low power state
+        // the mcu should be powered down and wait fot external interrupts
+        LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+    } else if (state == STATE_CHARGING) {
 
-        }
-    }
+    } else if (state == STATE_CHARGING_FINISHED) {
 
-    // CHECK TIMEOUTS
-    if (millis() > shutdown_timeout) { //the raspberry has not answered the shutdown command
-        //hard reset the gameboy
+    } else if (state == STATE_RPI_ON) {
+
+    } else if (state == STATE_BAT_TEST) {
+
     }
 
     // CHECK SERIAL CONNECTION
@@ -111,5 +142,10 @@ void loop() {
         for (uint8_t i = 0; cmd_char != '\n'; i++) {
             cmd_buffer[i] = cmd_char;
         }
+    }
+
+    // CHECK TIMEOUTS
+    if (millis() > shutdown_timeout) { //the raspberry has not answered the shutdown command
+        //hard reset the gameboy
     }
 }
