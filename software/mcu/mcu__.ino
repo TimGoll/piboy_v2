94019f3c48473987bed17fd93ec4bc91ab24ba31
TimDerGoll <tim@timgoll.de>
2018-10-31 13:43:26 +0100

    started rewrite of MCU code

---
+++ software/mcu/mcu__.ino
@@ -0,0 +1,164 @@
+#include "LowPower.h"
+
+#include "MAX17043.h"
+#include "Wire.h"
+
+/*
+ * There are four different states:
+ *(0: LOW PWR           : low power mode)
+ * 1: CHARGING          : displays current charging state on the LEDs
+ * 2: CHARGING_FINISHED : lets all LEDs blink
+ * 3: RPI_ON            : raspbery pi is on and the MAX17043 IC monitors the current voltage
+ * 4: BAT_TEST          : display current battery voltage state
+ *
+ * There are five different events:
+ * 10: CHARGING STOPPED
+ * 11: CHARGING STARTED
+ * 20: RPI_PWR_OFF
+ * 21: RPI_PWR_ON
+ * 31: REQUEST_BAT_STATE
+ */
+
+/*
+ * the states should be devided into states and substates
+ * states: raspbery pi on / raspbery pi off
+ * substates: default / charging / charging finished / battery state request
+ * (always returns into default after they are finished)
+ */
+
+#define BAUDRATE 115200         //baudrate for uart between raspberry and arduino
+#define THRESHOLD 30            //debouncetime for buttons
+#define SHUTDOWN_TIMEOUT = 1*60 //timeout for shutdown response
+
+//interrupt pins are able to awake the mcu from its low power mode
+#define PIN_INT_BATTEST 2
+#define PIN_INT_ONOFF 3
+#define PIN_INT_CHARGE 999
+
+//state IDs
+#define STATE_LOW_PWR 0
+#define STATE_CHARGING 1
+#define STATE_CHARGING_FINISHED 2
+#define STATE_RPI_ON 3
+#define STATE_BAT_TEST 4
+
+//event flags
+bool event_charging_started       = false;
+bool event_charging_stoppend      = false;
+bool event_rpi_power_on           = false;
+bool event_rpi_power_off          = false;
+bool event_request_bat_state      = false;
+
+//timing variables
+uint32_t time_last_bat_state_event = 0;
+uint32_t time_last_on_off_event    = 0;
+
+uint32_t time_shutdown_timeout     = 0;
+
+/******************************************************************************/
+/* INTERRUPT CALLBACK FUNCTIONS ***********************************************/
+/******************************************************************************/
+
+void int_battest() { //negative edge (inverted logic)
+    if (millis() - time_last_bat_state_event > THRESHOLD) {
+        time_last_bat_state_event = millis();
+        event_request_bat_state   = true;
+    }
+}
+
+void int_onoff() { //positive & negative edge
+    if (millis() - time_last_on_off_event > THRESHOLD) {
+        time_last_on_off_event = millis();
+        if (digitalRead(PIN_INT_ONOFF))
+            event_rpi_power_on = true;
+        else
+            event_rpi_power_off = true;
+    }
+}
+
+void int_charge() {
+
+}
+
+/******************************************************************************/
+/* EVENT HANDLE FUNCTIONS *****************************************************/
+/******************************************************************************/
+
+void handle_event_request_bat_state() {
+    event_request_bat_state = false;
+
+    if (state == STATE_CHARGING)
+        return;
+    if (state == STATE_CHARGING_FINISHED)
+        return;
+
+    state = STATE_BAT_TEST;
+}
+
+/******************************************************************************/
+/* MAIN FUNCTIONS *************************************************************/
+/******************************************************************************/
+
+void setup() {
+    //setup serial communication with the Raspberry Pi
+    Serial.begin(BAUDRATE);
+
+    //pinModes
+    pinMode(PIN_INT_BATTEST, INPUT_PULLUP);
+    pinMode(PIN_INT_ONOFF,   INPUT_PULLUP);
+
+    //attach interrupts; interrupts stop low power mode
+    attachInterrupt(PIN_INT_BATTEST, int_battest, LOW);
+    attachInterrupt(PIN_INT_ONOFF,   int_onoff,   CHANGE);
+    //TODO more wake ups
+
+    //setup fuel gauge
+    Wire.begin();
+    batteryMonitor.reset(); //TODO reset needed?
+    batteryMonitor.quickStart();
+
+    //enter low power mode
+    LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+}
+
+void loop() {
+    // CHECK EVENT FLAGS
+    if (event_request_bat_state) {
+        handle_event_request_bat_state();
+    }
+
+    // SWITCH STATES
+    if (state == STATE_LOW_PWR) {
+        // state 0 is the low power state
+        // the mcu should be powered down and wait fot external interrupts
+        LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+    } else if (state == STATE_CHARGING) {
+        // in this state the MCU is turned on, measures the current voltage and
+        // displays the current charging state on the LEDs
+    } else if (state == STATE_CHARGING_FINISHED) {
+        // after finishing the charging process, all LEDs start to blink until
+        // the charging cable is removed; the MCU shuts down after removal of the
+        // charging cable
+    } else if (state == STATE_RPI_ON) {
+        // MCU is turned on and monitors the battery voltage
+        // if the voltage falls below a certain threshold, the MCU sends a message
+        // via serial to the raspberry pi
+    } else if (state == STATE_BAT_TEST) {
+        // the previous state is important here!
+        // the MCU is turned on for 5 seconds and displays the battery state
+    }
+
+    // CHECK SERIAL CONNECTION
+    if (Serial.available() > 0) {
+        char cmd_buffer[16];
+        char cmd_char = Serial.read()
+        for (uint8_t i = 0; cmd_char != '\n'; i++) {
+            cmd_buffer[i] = cmd_char;
+        }
+    }
+
+    // CHECK TIMEOUTS
+    if (millis() > shutdown_timeout) { //the raspberry has not answered the shutdown command
+        //hard reset the gameboy
+    }
+}
